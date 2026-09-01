3f85dea379ba361c30e57c8a4c708d2033830340
조성우 <chomuscleguy@gmail.com>
2026-09-01 20:26:24 +0900

    fix: 테스트 환경에서 Google GenAI API 키 누락 문제 해결

--- src/test/java/com/chomu/raspiaiagent/RaspiAiAgentApplicationTests.java
+++ src/test/java/com/chomu/raspiaiagent/RaspiAiAgentApplicationTests.java
@@ -3,11 +3,12 @@ package com.chomu.raspiaiagent;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.ai.google.genai.api-key=test-dummy-key"
+})
 class RaspiAiAgentApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
